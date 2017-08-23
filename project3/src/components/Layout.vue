7c26fb4cbfd39a9c42ae728373f3cebd1a327eac
zhangjin <jin.zhang8@pactera.com>
2017-08-23 10:37:04 +0800

    项目框架

---
+++ project3/src/components/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="header"></div>
+    <div class="content">
+      <router-view></router-view>
+      <div>{{mag}}</div>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      msg: '123456'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ project3/src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    123343222
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
